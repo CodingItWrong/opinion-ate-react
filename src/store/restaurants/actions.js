fb0225fc4a21047bfec937cdad3c0315cfc727c6
Josh Justice <me@codingitwrong.com>
2022-04-13 08:35:21 -0400

    Implement Redux store to load and store records

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,1 +1,12 @@
-export const loadRestaurants = () => () => {};
+export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+
+export const loadRestaurants = () => (dispatch, getState, api) => {
+  api.loadRestaurants().then(records => {
+    dispatch(storeRestaurants(records));
+  });
+};
+
+const storeRestaurants = records => ({
+  type: STORE_RESTAURANTS,
+  records,
+});
--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,6 +1,14 @@
 import {combineReducers} from 'redux';
+import {STORE_RESTAURANTS} from './actions';
 
-const records = () => [];
+const records = (state = [], action) => {
+  switch (action.type) {
+    case STORE_RESTAURANTS:
+      return action.records;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   records,
