873af63780e88938ec0c5b1a10fa17fb412fd649
Josh Justice <me@codingitwrong.com>
2022-04-13 07:21:27 -0400

    Display restaurants in list

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,11 +1,17 @@
 import {useEffect} from 'react';
 
-export const RestaurantList = ({loadRestaurants}) => {
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <ul>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default RestaurantList;
