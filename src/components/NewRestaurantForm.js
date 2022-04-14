e4eaee2a83704df512ab265efaa36a0dc21029c4
Josh Justice <me@codingitwrong.com>
2022-04-14 07:07:46 -0400

    Test clicking button

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -5,7 +5,11 @@ export const NewRestaurantForm = () => {
   return (
     <form>
       <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        data-testid="new-restaurant-submit-button"
+      >
         Add
       </Button>
     </form>
---
+++ src/components/__tests__/NewRestaurantForm.spec.js
@@ -0,0 +1,28 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {NewRestaurantForm} from '../NewRestaurantForm';
+
+describe('NewRestaurantForm', () => {
+  const restaurantName = 'Sushi Place';
+
+  let createRestaurant;
+
+  beforeEach(() => {
+    createRestaurant = jest.fn().mockName('createRestaurant');
+    render(<NewRestaurantForm createRestaurant={createRestaurant} />);
+  });
+
+  describe('when filled in', () => {
+    beforeEach(async () => {
+      await userEvent.type(
+        screen.getByPlaceholderText('Add Restaurant'),
+        restaurantName,
+      );
+      userEvent.click(screen.getByTestId('new-restaurant-submit-button'));
+    });
+
+    it('calls createRestaurant with the name', () => {
+      expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
+    });
+  });
+});
