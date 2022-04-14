439b4a1633aaba32a89d65989e17c8ababe40d0a
Josh Justice <me@codingitwrong.com>
2022-04-14 08:23:24 -0400

    Test store rejects

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -165,5 +165,13 @@ describe('restaurants', () => {
         return expect(promise).resolves.toBeUndefined();
       });
     });
+
+    describe('when save fails', () => {
+      it('rejects', () => {
+        api.createRestaurant.mockRejectedValue();
+        promise = store.dispatch(createRestaurant(newRestaurantName));
+        return expect(promise).rejects.toBeUndefined();
+      });
+    });
   });
 });
