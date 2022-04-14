f6ff304f931fa70a3b91faafe74a823c4a88aaa0
Josh Justice <me@codingitwrong.com>
2022-04-14 07:09:26 -0400

    Call submit function

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -1,11 +1,17 @@
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-export const NewRestaurantForm = () => {
+export const NewRestaurantForm = ({createRestaurant}) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    createRestaurant();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
       <Button
+        type="submit"
         variant="contained"
         color="primary"
         data-testid="new-restaurant-submit-button"
