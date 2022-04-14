3a534bfa89bdf86a266c64d0cb59735569720150
Josh Justice <me@codingitwrong.com>
2022-04-14 07:04:08 -0400

    Add submit button

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -1,9 +1,13 @@
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export const NewRestaurantForm = () => {
   return (
     <form>
       <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
+      <Button variant="contained" color="primary">
+        Add
+      </Button>
     </form>
   );
 };
