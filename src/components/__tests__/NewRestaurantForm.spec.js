d995bc4245a1e12f3b2e32d6dfa3f063bc2a96ca
Josh Justice <me@codingitwrong.com>
2022-04-19 07:26:47 -0400

    Switch success test to a waitFor

--- src/components/__tests__/NewRestaurantForm.spec.js
+++ src/components/__tests__/NewRestaurantForm.spec.js
@@ -1,6 +1,5 @@
-import {act, render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import flushPromises from 'flush-promises';
 import {NewRestaurantForm} from '../NewRestaurantForm';
 
 describe('NewRestaurantForm', () => {
@@ -15,25 +14,19 @@ describe('NewRestaurantForm', () => {
     render(<NewRestaurantForm createRestaurant={createRestaurant} />);
   });
 
-  describe('when filled in', () => {
-    beforeEach(async () => {
-      createRestaurant.mockResolvedValue();
-      await userEvent.type(
-        screen.getByPlaceholderText('Add Restaurant'),
-        restaurantName,
-      );
-      userEvent.click(screen.getByTestId('new-restaurant-submit-button'));
+  it('submits the form when filled in', async () => {
+    createRestaurant.mockResolvedValue();
+    await userEvent.type(
+      screen.getByPlaceholderText('Add Restaurant'),
+      restaurantName,
+    );
+    userEvent.click(screen.getByTestId('new-restaurant-submit-button'));
 
-      return act(flushPromises);
-    });
+    expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
 
-    it('calls createRestaurant with the name', () => {
-      expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
-    });
-
-    it('clears the name', () => {
-      expect(screen.getByPlaceholderText('Add Restaurant').value).toEqual('');
-    });
+    await waitFor(() =>
+      expect(screen.getByPlaceholderText('Add Restaurant').value).toEqual(''),
+    );
   });
 
   describe('when empty', () => {
