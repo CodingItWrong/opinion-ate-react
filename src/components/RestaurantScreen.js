76699ac3114bc6db3706a80d2ad5ef05922ada72
Josh Justice <me@codingitwrong.com>
2022-04-29 16:24:38 -0400

    Style RestaurantScreen

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,10 +1,15 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import RestaurantList from './RestaurantList';
 
 export default function RestaurantScreen() {
   return (
-    <div>
-      <h1>Restaurants</h1>
-      <RestaurantList />
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList />
+      </CardContent>
+    </Card>
   );
 }
