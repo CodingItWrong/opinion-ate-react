ae404c0579a777a4a48bce2fadfd936f735866f4
Josh Justice <me@codingitwrong.com>
2022-04-29 15:00:18 -0400

    SPecify app should list restaurants

---
+++ cypress/integration/listing-restaurants.spec.js
@@ -0,0 +1,19 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.intercept(
+      'GET',
+      'https://outside-in-dev-api.herokuapp.com/*/restaurants',
+      [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    );
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
