ab3b442aed556262cd1792357e04fb623a37dd87
Josh Justice <me@codingitwrong.com>
2022-04-29 15:04:24 -0400

    Add RestaurantScreen and RestaurantList

---
+++ src/components/RestaurantScreen.js
@@ -0,0 +1,10 @@
+import RestaurantList from './RestaurantList';
+
+export default function RestaurantScreen() {
+  return (
+    <div>
+      <h1>Restaurants</h1>
+      <RestaurantList />
+    </div>
+  );
+}
