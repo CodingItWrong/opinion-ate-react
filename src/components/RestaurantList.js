4fd5cfaaebed93f3b8d45d57be11bc1320e91cc2
Josh Justice <me@codingitwrong.com>
2022-04-29 15:23:36 -0400

    Set up Redux

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,8 @@
 import {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {loadRestaurants} from '../store/restaurants/actions';
 
-export default function RestaurantList({loadRestaurants, restaurants}) {
+export function RestaurantList({loadRestaurants, restaurants}) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
@@ -13,3 +15,11 @@ export default function RestaurantList({loadRestaurants, restaurants}) {
     </ul>
   );
 }
+
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
+
+const mapDispatchToProps = {loadRestaurants};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
