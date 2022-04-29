31bf3f380b2cd1993cca54d90938a83b0a572e0b
Josh Justice <me@codingitwrong.com>
2022-04-29 15:13:41 -0400

    Extract duplicate test setup

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -2,9 +2,14 @@ import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+  let loadRestaurants;
+
+  function renderComponent() {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
 
     render(
       <RestaurantList
@@ -12,19 +17,15 @@ describe('RestaurantList', () => {
         restaurants={restaurants}
       />,
     );
+  }
 
+  it('loads restaurants on first render', () => {
+    renderComponent();
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('displays the restaurants', () => {
-    const noop = () => {};
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
-
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
-
+    renderComponent();
     expect(screen.getByText('Sushi Place')).toBeInTheDocument();
     expect(screen.getByText('Pizza Place')).toBeInTheDocument();
   });
