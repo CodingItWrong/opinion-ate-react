fe96925abf256e785b8e98662cfa716fe57534d9
Josh Justice <me@codingitwrong.com>
2022-04-29 15:31:54 -0400

    Test store action to load data

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,1 +1,11 @@
-export const loadRestaurants = () => () => {};
+export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+
+export const loadRestaurants = () => async (dispatch, getState, api) => {
+  const records = await api.loadRestaurants();
+  dispatch(storeRestaurants(records));
+};
+
+const storeRestaurants = records => ({
+  type: STORE_RESTAURANTS,
+  records,
+});
