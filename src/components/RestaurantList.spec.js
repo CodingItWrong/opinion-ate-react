1c2d03e60fa624e37ad16c656ac42b4e332f86a6
Josh Justice <me@codingitwrong.com>
2022-04-29 15:12:09 -0400

    Display passed in restaurants

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,12 +1,31 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
   it('loads restaurants on first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    const restaurants = [];
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    const noop = () => {};
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+
+    expect(screen.getByText('Sushi Place')).toBeInTheDocument();
+    expect(screen.getByText('Pizza Place')).toBeInTheDocument();
   });
 });
