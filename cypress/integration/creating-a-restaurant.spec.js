d63fc1535188264241effa5657d1e8ee47c89638
Josh Justice <me@codingitwrong.com>
2022-04-30 08:47:56 -0400

    Specify creating a restaurant

---
+++ cypress/integration/creating-a-restaurant.spec.js
@@ -0,0 +1,32 @@
+describe('Creating a Restaurant', () => {
+  it('allows adding restaurants', () => {
+    const restaurantId = 27;
+    const restaurantName = 'Sushi Place';
+
+    cy.intercept(
+      'GET',
+      'https://outside-in-dev-api.herokuapp.com/*/restaurants',
+      [],
+    );
+
+    cy.intercept(
+      'POST',
+      'https://outside-in-dev-api.herokuapp.com/*/restaurants',
+      {
+        id: restaurantId,
+        name: restaurantName,
+      },
+    ).as('addRestaurant');
+
+    cy.visit('/');
+
+    cy.get('[placeholder="Add Restaurant"]').type(restaurantName);
+    cy.contains('Add').click();
+
+    cy.wait('@addRestaurant').its('requestBody').should('deep.equal', {
+      name: restaurantName,
+    });
+
+    cy.contains(restaurantName);
+  });
+});
