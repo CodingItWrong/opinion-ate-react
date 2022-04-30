ab86d4928b449405092f1d6b0fff37452466d18c
Josh Justice <me@codingitwrong.com>
2022-04-30 07:53:23 -0400

    Start loading as false

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,9 +1,13 @@
+export const START_LOADING = 'START_LOADING';
 export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
 
 export const loadRestaurants = () => async (dispatch, getState, api) => {
+  dispatch(startLoading());
   const records = await api.loadRestaurants();
   dispatch(storeRestaurants(records));
 };
+
+const startLoading = () => ({type: START_LOADING});
 
 const storeRestaurants = records => ({
   type: STORE_RESTAURANTS,
--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import {STORE_RESTAURANTS} from './actions';
+import {START_LOADING, STORE_RESTAURANTS} from './actions';
 
 function records(state = [], action) {
   switch (action.type) {
@@ -10,8 +10,10 @@ function records(state = [], action) {
   }
 }
 
-function loading(state = true, action) {
+function loading(state = false, action) {
   switch (action.type) {
+    case START_LOADING:
+      return true;
     case STORE_RESTAURANTS:
       return false;
     default:
