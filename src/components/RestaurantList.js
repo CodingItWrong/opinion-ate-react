67a5c8dac342fa925fd33ddc0ae14c9f2e8df313
Josh Justice <me@codingitwrong.com>
2022-04-30 08:28:15 -0400

    Hook up loadError property

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -37,6 +37,7 @@ export function RestaurantList({
 const mapStateToProps = state => ({
   restaurants: state.restaurants.records,
   loading: state.restaurants.loading,
+  loadError: state.restaurants.loadError,
 });
 
 const mapDispatchToProps = {loadRestaurants};
