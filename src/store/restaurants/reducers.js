2243a34e341afadcae22063afd7844dc31a98ad3
Josh Justice <me@codingitwrong.com>
2022-04-30 08:20:41 -0400

    Start with no loading error

--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,5 +1,9 @@
 import {combineReducers} from 'redux';
-import {START_LOADING, STORE_RESTAURANTS} from './actions';
+import {
+  START_LOADING,
+  STORE_RESTAURANTS,
+  RECORD_LOADING_ERROR,
+} from './actions';
 
 function records(state = [], action) {
   switch (action.type) {
@@ -21,8 +25,13 @@ function loading(state = false, action) {
   }
 }
 
-function loadError() {
-  return true;
+function loadError(state = false, action) {
+  switch (action.type) {
+    case RECORD_LOADING_ERROR:
+      return true;
+    default:
+      return state;
+  }
 }
 
 export default combineReducers({
