7984ea56c05563b880b3d13ac9843cdd9e97a3a8
Josh Justice <me@codingitwrong.com>
2022-05-03 06:23:44 -0400

    Nest tests

--- src/components/RestaurantScreen.spec.js
+++ src/components/RestaurantScreen.spec.js
@@ -1,72 +1,74 @@
 import {loadRestaurantsPure} from './RestaurantScreen';
 
-describe('loadRestaurantsPure', () => {
-  let api;
-  let setRestaurants;
-  let setLoading;
-  let setLoadError;
-
-  beforeEach(() => {
-    setRestaurants = jest.fn().mockName('setRestaurants');
-    setLoading = jest.fn().mockName('setLoading');
-    setLoadError = jest.fn().mockName('setLoadError');
-    api = {
-      loadRestaurants: jest.fn().mockName('api.loadRestaurants'),
-    };
-  });
-
-  function call() {
-    loadRestaurantsPure({api, setRestaurants, setLoading, setLoadError});
-  }
-
-  describe('while loading', () => {
-    beforeEach(() => {
-      const doesNotResolve = new Promise(() => {});
-      api.loadRestaurants.mockImplementation(doesNotResolve);
-      call();
-    });
-
-    it('sets the loading flag', () => {
-      expect(setLoading).toHaveBeenCalledWith(true);
-    });
-
-    it('clears the error flag', () => {
-      expect(setLoadError).toHaveBeenCalledWith(false);
-    });
-  });
-
-  describe('when loading succeeds', () => {
-    const records = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
+describe('RestaurantScreen', () => {
+  describe('loadRestaurantsPure', () => {
+    let api;
+    let setRestaurants;
+    let setLoading;
+    let setLoadError;
 
     beforeEach(() => {
-      api.loadRestaurants.mockResolvedValue(records);
-      call();
+      setRestaurants = jest.fn().mockName('setRestaurants');
+      setLoading = jest.fn().mockName('setLoading');
+      setLoadError = jest.fn().mockName('setLoadError');
+      api = {
+        loadRestaurants: jest.fn().mockName('api.loadRestaurants'),
+      };
     });
 
-    it('stores the restaurants', () => {
-      expect(setRestaurants).toHaveBeenCalledWith(records);
+    function call() {
+      loadRestaurantsPure({api, setRestaurants, setLoading, setLoadError});
+    }
+
+    describe('while loading', () => {
+      beforeEach(() => {
+        const doesNotResolve = new Promise(() => {});
+        api.loadRestaurants.mockImplementation(doesNotResolve);
+        call();
+      });
+
+      it('sets the loading flag', () => {
+        expect(setLoading).toHaveBeenCalledWith(true);
+      });
+
+      it('clears the error flag', () => {
+        expect(setLoadError).toHaveBeenCalledWith(false);
+      });
     });
 
-    it('clears the loading flag', () => {
-      expect(setLoading).toHaveBeenCalledWith(false);
-    });
-  });
+    describe('when loading succeeds', () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
 
-  describe('when loading fails', () => {
-    beforeEach(() => {
-      api.loadRestaurants.mockRejectedValue();
-      return call();
+      beforeEach(() => {
+        api.loadRestaurants.mockResolvedValue(records);
+        call();
+      });
+
+      it('stores the restaurants', () => {
+        expect(setRestaurants).toHaveBeenCalledWith(records);
+      });
+
+      it('clears the loading flag', () => {
+        expect(setLoading).toHaveBeenCalledWith(false);
+      });
     });
 
-    it('sets an error flag', () => {
-      expect(setLoadError).toHaveBeenCalledWith(true);
-    });
+    describe('when loading fails', () => {
+      beforeEach(() => {
+        api.loadRestaurants.mockRejectedValue();
+        return call();
+      });
 
-    it('clears the loading flag', () => {
-      expect(setLoading).toHaveBeenCalledWith(false);
+      it('sets an error flag', () => {
+        expect(setLoadError).toHaveBeenCalledWith(true);
+      });
+
+      it('clears the loading flag', () => {
+        expect(setLoading).toHaveBeenCalledWith(false);
+      });
     });
   });
 });
