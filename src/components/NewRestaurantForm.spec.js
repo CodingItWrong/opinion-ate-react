95e140e5b41b04e65f93ff23b622ba5f56c12be1
Josh Justice <me@codingitwrong.com>
2022-05-18 07:07:16 -0400

    Fix component tests

--- src/components/NewRestaurantForm.spec.js
+++ src/components/NewRestaurantForm.spec.js
@@ -1,7 +1,7 @@
 import {act, render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import flushPromises from 'flush-promises';
-import {NewRestaurantForm} from './NewRestaurantForm';
+import NewRestaurantForm from './NewRestaurantForm';
 
 describe('NewRestaurantForm', () => {
   const restaurantName = 'Sushi Place';
