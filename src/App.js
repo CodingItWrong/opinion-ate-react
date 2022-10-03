cc775847a53b15f330ad658bd7ec1a82de893d0a
Josh Justice <me@codingitwrong.com>
2022-10-03 07:47:10 -0400

    Wire up reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
 export default function App() {
   return (
-    <div>
+    <Provider store={store}>
       <RestaurantScreen />
-    </div>
+    </Provider>
   );
 }
---
+++ src/store/restaurants/reducers.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+function records() {
+  return [];
+}
+
+export default combineReducers({
+  records,
+});
