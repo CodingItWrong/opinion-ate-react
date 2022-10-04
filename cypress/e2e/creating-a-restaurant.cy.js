686f4793e8e129f1786ec64bef2451e1a5bd189f
Josh Justice <me@codingitwrong.com>
2022-10-04 06:10:20 -0400

    Specify adding a restaurant

---
+++ cypress/e2e/creating-a-restaurant.cy.js
@@ -0,0 +1,24 @@
+describe('Creating a Restaurant', () => {
+  it('allows adding restaurants', () => {
+    const restaurantId = 27;
+    const restaurantName = 'Sushi Place';
+
+    cy.intercept('GET', 'https://api.outsidein.dev/*/restaurants', []);
+
+    cy.intercept('POST', 'https://api.outsidein.dev/*/restaurants', {
+      id: restaurantId,
+      name: restaurantName,
+    }).as('addRestaurant');
+
+    cy.visit('/');
+
+    cy.get('[placeholder="Add Restaurant"]').type(restaurantName);
+    cy.contains('Add').click();
+
+    cy.wait('@addRestaurant')
+      .its('request.body')
+      .should('deep.equal', {name: restaurantName});
+
+    cy.contains(restaurantName);
+  });
+});
