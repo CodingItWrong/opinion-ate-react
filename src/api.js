18699e1a206505b9d7951c77d06f236c378d0aeb
Josh Justice <me@codingitwrong.com>
2022-10-04 06:22:32 -0400

    Add API method to create restaurant

--- src/api.js
+++ src/api.js
@@ -9,6 +9,10 @@ const api = {
     const response = await client.get('/restaurants');
     return response.data;
   },
+  async createRestaurant(name) {
+    const response = await client.post('/restaurants', {name});
+    return response.data;
+  },
 };
 
 export default api;
